46a4ab7fbf924b7ee3a8036ef90e3be13dfebe05
JiayuLin <linjiayu@tju.edu.cn>
2019-07-05 12:03:36 +0800

    注意考虑问题的全面性

--- 19_RegularExpressionsMatching/RegularExpressions.cpp
+++ 19_RegularExpressionsMatching/RegularExpressions.cpp
@@ -22,11 +22,46 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 
 bool matchCore(const char* str, const char* pattern);
 
+// bool match(const char* str, const char* pattern)
+// {
+//     if(str == nullptr || pattern == nullptr)
+//         return false;
+
+//     return matchCore(str, pattern);
+// }
+
+// bool matchCore(const char* str, const char* pattern)
+// {
+//     if(*str == '\0' && *pattern == '\0')
+//         return true;
+
+//     if(*str != '\0' && *pattern == '\0')
+//         return false;
+
+//     if(*(pattern + 1) == '*')
+//     {
+//         if(*pattern == *str || (*pattern == '.' && *str != '\0'))
+//             // 进入有限状态机的下一个状态
+//             return matchCore(str + 1, pattern + 2)
+//             // 继续留在有限状态机的当前状态 
+//             || matchCore(str + 1, pattern)
+//             // 略过一个'*' 
+//             || matchCore(str, pattern + 2);
+//         else
+//             // 略过一个'*'
+//             return matchCore(str, pattern + 2);
+//     }
+
+//     if(*str == *pattern || (*pattern == '.' && *str != '\0'))
+//         return matchCore(str + 1, pattern + 1);
+
+//     return false;
+// }
+
 bool match(const char* str, const char* pattern)
 {
-    if(str == nullptr || pattern == nullptr)
+    if (str == nullptr || pattern == nullptr)
         return false;
-
     return matchCore(str, pattern);
 }
 
@@ -34,30 +69,23 @@ bool matchCore(const char* str, const char* pattern)
 {
     if(*str == '\0' && *pattern == '\0')
         return true;
-
-    if(*str != '\0' && *pattern == '\0')
+    if(*str != '\0' && *pattern == '\0') /* 说明长度不一致 */
         return false;
-
-    if(*(pattern + 1) == '*')
+    if(*(pattern+1) == '*')
     {
-        if(*pattern == *str || (*pattern == '.' && *str != '\0'))
-            // 进入有限状态机的下一个状态
-            return matchCore(str + 1, pattern + 2)
-            // 继续留在有限状态机的当前状态 
-            || matchCore(str + 1, pattern)
-            // 略过一个'*' 
-            || matchCore(str, pattern + 2);
+        if(*str == *pattern || (*str != '\0' && *pattern == '.'))
+        {
+            return matchCore(str+1,pattern+2)
+                || matchCore(str+1,pattern)
+                || matchCore(str,pattern+2);
+        } 
         else
-            // 略过一个'*'
-            return matchCore(str, pattern + 2);
+            return matchCore(str, pattern+2);
     }
-
-    if(*str == *pattern || (*pattern == '.' && *str != '\0'))
-        return matchCore(str + 1, pattern + 1);
-
+    if(*str == *pattern || (*str != '\0' && *pattern == '.'))
+        return matchCore(str+1, pattern+1);
     return false;
 }
-
 // ====================测试代码====================
 void Test(const char* testName, const char* string, const char* pattern, bool expected)
 {
