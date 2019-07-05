65e414fa1ef61329b28d55f9e23fe6256ef24850
JiayuLin <linjiayu@tju.edu.cn>
2019-07-05 17:53:03 +0800

    前后两指针往中间遍历的方法

--- 21_ReorderArray/ReorderArray.cpp
+++ 21_ReorderArray/ReorderArray.cpp
@@ -22,30 +22,52 @@ void Reorder(int *pData, unsigned int length, bool (*func)(int));
 bool isEven(int n);
 
 // ====================方法一====================
+// void ReorderOddEven_1(int *pData, unsigned int length)
+// {
+//     if(pData == nullptr || length == 0)
+//         return;
+
+//     int *pBegin = pData;
+//     int *pEnd = pData + length - 1;
+
+//     while(pBegin < pEnd)
+//     {
+//         // 向后移动pBegin，直到它指向偶数
+//         while(pBegin < pEnd && (*pBegin & 0x1) != 0)
+//             pBegin ++;
+
+//         // 向前移动pEnd，直到它指向奇数
+//         while(pBegin < pEnd && (*pEnd & 0x1) == 0)
+//             pEnd --;
+
+//         if(pBegin < pEnd)
+//         {
+//             int temp = *pBegin;
+//             *pBegin = *pEnd;
+//             *pEnd = temp;
+//         }
+//     }
+// }
+
 void ReorderOddEven_1(int *pData, unsigned int length)
 {
     if(pData == nullptr || length == 0)
         return;
-
     int *pBegin = pData;
     int *pEnd = pData + length - 1;
 
     while(pBegin < pEnd)
     {
-        // 向后移动pBegin，直到它指向偶数
-        while(pBegin < pEnd && (*pBegin & 0x1) != 0)
-            pBegin ++;
-
-        // 向前移动pEnd，直到它指向奇数
-        while(pBegin < pEnd && (*pEnd & 0x1) == 0)
-            pEnd --;
-
+        while(!isEven(*pBegin))
+            ++pBegin;
+        while(isEven(*pEnd))
+            --pEnd;
         if(pBegin < pEnd)
         {
             int temp = *pBegin;
             *pBegin = *pEnd;
             *pEnd = temp;
-        }
+        }        
     }
 }
 
