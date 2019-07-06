df4cc9eb093cc3b0e8e7cb8df60a47083799e83a
JiayuLin <linjiayu@tju.edu.cn>
2019-07-06 21:59:38 +0800

    main must return int?

--- 28_SymmetricalBinaryTree/SymmetricalBinaryTree.cpp
+++ 28_SymmetricalBinaryTree/SymmetricalBinaryTree.cpp
@@ -17,7 +17,84 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 // 它的镜像一样，那么它是对称的。
 
 #include <cstdio>
-#include "../Utilities/BinaryTree.h"
+//#include "../Utilities/BinaryTree.h"
+struct BinaryTreeNode 
+{
+    int                    m_nValue; 
+    BinaryTreeNode*        m_pLeft;  
+    BinaryTreeNode*        m_pRight; 
+};
+BinaryTreeNode* CreateBinaryTreeNode(int value)
+{
+    BinaryTreeNode* pNode = new BinaryTreeNode();
+    pNode->m_nValue = value;
+    pNode->m_pLeft = nullptr;
+    pNode->m_pRight = nullptr;
+
+    return pNode;
+}
+
+void ConnectTreeNodes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight)
+{
+    if(pParent != nullptr)
+    {
+        pParent->m_pLeft = pLeft;
+        pParent->m_pRight = pRight;
+    }
+}
+
+void PrintTreeNode(const BinaryTreeNode* pNode)
+{
+    if(pNode != nullptr)
+    {
+        printf("value of this node is: %d\n", pNode->m_nValue);
+
+        if(pNode->m_pLeft != nullptr)
+            printf("value of its left child is: %d.\n", pNode->m_pLeft->m_nValue);
+        else
+            printf("left child is nullptr.\n");
+
+        if(pNode->m_pRight != nullptr)
+            printf("value of its right child is: %d.\n", pNode->m_pRight->m_nValue);
+        else
+            printf("right child is nullptr.\n");
+    }
+    else
+    {
+        printf("this node is nullptr.\n");
+    }
+
+    printf("\n");
+}
+
+void PrintTree(const BinaryTreeNode* pRoot)
+{
+    PrintTreeNode(pRoot);
+
+    if(pRoot != nullptr)
+    {
+        if(pRoot->m_pLeft != nullptr)
+            PrintTree(pRoot->m_pLeft);
+
+        if(pRoot->m_pRight != nullptr)
+            PrintTree(pRoot->m_pRight);
+    }
+}
+
+void DestroyTree(BinaryTreeNode* pRoot)
+{
+    if(pRoot != nullptr)
+    {
+        BinaryTreeNode* pLeft = pRoot->m_pLeft;
+        BinaryTreeNode* pRight = pRoot->m_pRight;
+
+        delete pRoot;
+        pRoot = nullptr;
+
+        DestroyTree(pLeft);
+        DestroyTree(pRight);
+    }
+}
 
 bool isSymmetrical(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2);
 
@@ -26,20 +103,35 @@ bool isSymmetrical(BinaryTreeNode* pRoot)
     return isSymmetrical(pRoot, pRoot);
 }
 
+// bool isSymmetrical(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2)
+// {
+//     if(pRoot1 == nullptr && pRoot2 == nullptr)
+//         return true;
+
+//     if(pRoot1 == nullptr || pRoot2 == nullptr)
+//         return false;
+
+//     if(pRoot1->m_nValue != pRoot2->m_nValue)
+//         return false;
+
+//     return isSymmetrical(pRoot1->m_pLeft, pRoot2->m_pRight)
+//         && isSymmetrical(pRoot1->m_pRight, pRoot2->m_pLeft);
+// }
+
 bool isSymmetrical(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2)
 {
     if(pRoot1 == nullptr && pRoot2 == nullptr)
         return true;
-
+    /* 能运行这行说明至少有一个指针不为空 */
     if(pRoot1 == nullptr || pRoot2 == nullptr)
         return false;
-
     if(pRoot1->m_nValue != pRoot2->m_nValue)
         return false;
 
     return isSymmetrical(pRoot1->m_pLeft, pRoot2->m_pRight)
         && isSymmetrical(pRoot1->m_pRight, pRoot2->m_pLeft);
 }
+
 
 // ====================测试代码====================
 void Test(char* testName, BinaryTreeNode* pRoot, bool expected)
@@ -297,7 +389,7 @@ void Test10()
     DestroyTree(pNode1);
 }
 
-void main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
     Test1();
     Test2();
@@ -309,4 +401,6 @@ void main(int argc, char* argv[])
     Test8();
     Test9();
     Test10();
+
+    return 0;
 }
