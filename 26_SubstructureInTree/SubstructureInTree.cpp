eec3ccb526e7f25270e112c0e413cca9a7d23418
JiayuLin <linjiayu@tju.edu.cn>
2019-07-06 19:57:13 +0800

    注意递归终止条件，值类型为double时，不能简单使用等号判断，可用两数之差绝对值小于某个范围来判断

--- 26_SubstructureInTree/SubstructureInTree.cpp
+++ 26_SubstructureInTree/SubstructureInTree.cpp
@@ -27,6 +27,38 @@ struct BinaryTreeNode
 bool DoesTree1HaveTree2(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2);
 bool Equal(double num1, double num2);
 
+// bool HasSubtree(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2)
+// {
+//     bool result = false;
+
+//     if(pRoot1 != nullptr && pRoot2 != nullptr)
+//     {
+//         if(Equal(pRoot1->m_dbValue, pRoot2->m_dbValue))
+//             result = DoesTree1HaveTree2(pRoot1, pRoot2);
+//         if(!result)
+//             result = HasSubtree(pRoot1->m_pLeft, pRoot2);
+//         if(!result)
+//             result = HasSubtree(pRoot1->m_pRight, pRoot2);
+//     }
+
+//     return result;
+// }
+
+// bool DoesTree1HaveTree2(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2)
+// {
+//     if(pRoot2 == nullptr)
+//         return true;
+
+//     if(pRoot1 == nullptr)
+//         return false;
+
+//     if(!Equal(pRoot1->m_dbValue, pRoot2->m_dbValue))
+//         return false;
+
+//     return DoesTree1HaveTree2(pRoot1->m_pLeft, pRoot2->m_pLeft) &&
+//         DoesTree1HaveTree2(pRoot1->m_pRight, pRoot2->m_pRight);
+// }
+
 bool HasSubtree(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2)
 {
     bool result = false;
@@ -48,15 +80,13 @@ bool DoesTree1HaveTree2(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2)
 {
     if(pRoot2 == nullptr)
         return true;
-
     if(pRoot1 == nullptr)
         return false;
-
     if(!Equal(pRoot1->m_dbValue, pRoot2->m_dbValue))
         return false;
 
-    return DoesTree1HaveTree2(pRoot1->m_pLeft, pRoot2->m_pLeft) &&
-        DoesTree1HaveTree2(pRoot1->m_pRight, pRoot2->m_pRight);
+    return DoesTree1HaveTree2(pRoot1->m_pLeft, pRoot2->m_pLeft)
+        && DoesTree1HaveTree2(pRoot1->m_pRight, pRoot2->m_pRight); 
 }
 
 bool Equal(double num1, double num2)
