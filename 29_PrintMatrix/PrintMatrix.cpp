c8b47291380556aa140b45511fdb822e6db6ba56
JiayuLin <linjiayu@tju.edu.cn>
2019-07-07 15:20:46 +0800

    当问题比较抽象时，通过画图来寻找规律

--- 29_PrintMatrix/PrintMatrix.cpp
+++ 29_PrintMatrix/PrintMatrix.cpp
@@ -20,17 +20,73 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 void PrintMatrixInCircle(int** numbers, int columns, int rows, int start);
 void printNumber(int number);
 
+// void PrintMatrixClockwisely(int** numbers, int columns, int rows)
+// {
+//     if(numbers == nullptr || columns <= 0 || rows <= 0)
+//         return;
+
+//     int start = 0;
+
+//     while(columns > start * 2 && rows > start * 2)
+//     {
+//         PrintMatrixInCircle(numbers, columns, rows, start);
+
+//         ++start;
+//     }
+// }
+
+// void PrintMatrixInCircle(int** numbers, int columns, int rows, int start)
+// {
+//     int endX = columns - 1 - start;
+//     int endY = rows - 1 - start;
+
+//     // 从左到右打印一行
+//     for(int i = start; i <= endX; ++i)
+//     {
+//         int number = numbers[start][i];
+//         printNumber(number);
+//     }
+
+//     // 从上到下打印一列
+//     if(start < endY)
+//     {
+//         for(int i = start + 1; i <= endY; ++i)
+//         {
+//             int number = numbers[i][endX];
+//             printNumber(number);
+//         }
+//     }
+
+//     // 从右到左打印一行
+//     if(start < endX && start < endY)
+//     {
+//         for(int i = endX - 1; i >= start; --i)
+//         {
+//             int number = numbers[endY][i];
+//             printNumber(number);
+//         }
+//     }
+
+//     // 从下到上打印一行
+//     if(start < endX && start < endY - 1)
+//     {
+//         for(int i = endY - 1; i >= start + 1; --i)
+//         {
+//             int number = numbers[i][start];
+//             printNumber(number);
+//         }
+//     }
+// }
+
 void PrintMatrixClockwisely(int** numbers, int columns, int rows)
 {
     if(numbers == nullptr || columns <= 0 || rows <= 0)
         return;
-
     int start = 0;
 
     while(columns > start * 2 && rows > start * 2)
     {
         PrintMatrixInCircle(numbers, columns, rows, start);
-
         ++start;
     }
 }
@@ -38,17 +94,15 @@ void PrintMatrixClockwisely(int** numbers, int columns, int rows)
 void PrintMatrixInCircle(int** numbers, int columns, int rows, int start)
 {
     int endX = columns - 1 - start;
-    int endY = rows - 1 - start;
+    int endY = rows - 1 -start;
 
-    // 从左到右打印一行
     for(int i = start; i <= endX; ++i)
     {
         int number = numbers[start][i];
         printNumber(number);
     }
 
-    // 从上到下打印一列
-    if(start < endY)
+    if(endY > start)
     {
         for(int i = start + 1; i <= endY; ++i)
         {
@@ -57,20 +111,18 @@ void PrintMatrixInCircle(int** numbers, int columns, int rows, int start)
         }
     }
 
-    // 从右到左打印一行
-    if(start < endX && start < endY)
+    if(endX > start && endY > start)
     {
-        for(int i = endX - 1; i >= start; --i)
+        for (int i = endX - 1; i >= start; --i)
         {
             int number = numbers[endY][i];
             printNumber(number);
         }
     }
 
-    // 从下到上打印一行
-    if(start < endX && start < endY - 1)
+    if(endY - 1 > start && endX > start)
     {
-        for(int i = endY - 1; i >= start + 1; --i)
+        for (int i = endY - 1; i >= start + 1; --i)
         {
             int number = numbers[i][start];
             printNumber(number);
